dde4185ced887561293a7e004574789d120405c1
gagn999@mail.ru <gagn999@mail.ru>
2023-12-25 12:34:58 +0500

    1

--- .venv/main.py
+++ .venv/main.py
@@ -1,12 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
 from application.routes import router
-
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://192.168.56.104"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # здесь указываем разрешённые адреса, символ "*"разрешает запросы для всех адресов
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(router)      # подключаем обработчик API URI
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
--- .venv/populate_db.py
+++ .venv/populate_db.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     with SessionLocal() as session:
         populate_station(session)
         populate_danger_type(session)
-        create_danger(session, temp_m=random.randint(1, 12), temp_frequency=random.randint(20, 800), station_id=1, danger_type=2)
+        create_danger(session, temp_m=random.randint(1, 12), temp_frequency=random.randint(20, 800), station_id=2, danger_type=1)
